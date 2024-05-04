2b6a4e2095c9f6876a1711b0f6bea3864552e251
Eugenia <164356917+Eugtoledo@users.noreply.github.com>
2024-05-04 08:07:47 -0300

    Vehiculo modifica comentarios con errores en javadoc

--- TP-Grupo11/src/negocio/Vehiculo.java
+++ TP-Grupo11/src/negocio/Vehiculo.java
@@ -1,23 +1,22 @@
 package negocio;
-
 /**
- * Esta clase abstracta representa un vehículo genérico utilizado en viajes.
- * Proporciona información sobre la patente, capacidad de carga, amigabilidad con mascotas y capacidad de pasajeros.
- * Las clases concretas que heredan de esta deben implementar los detalles específicos del vehículo.
+ * Esta clase abstracta representa un vehiculo generico utilizado en viajes.
+ * Proporciona informacion sobre la patente, capacidad de carga, amigabilidad con mascotas y capacidad de pasajeros.
+ * Las clases concretas que heredan de esta deben implementar los detalles especificos del vehiculo.
  */
 public abstract class Vehiculo {
     private String patente; // La patente del vehículo
-    private boolean baul; // Indica si el vehículo tiene espacio de carga (baúl)
-    private boolean petFriendly; // Indica si el vehículo es apto para mascotas
-    private int maxPasajeros; // El número máximo de pasajeros que puede transportar el vehículo
+    private boolean baul; // Indica si el vehiculo tiene espacio de carga (baul)
+    private boolean petFriendly; // Indica si el vehiculo es apto para mascotas
+    private int maxPasajeros; // El numero maximo de pasajeros que puede transportar el vehiculo
 
     /**
-     * Crea un nuevo objeto Vehiculo con los parámetros especificados.
+     * Crea un nuevo objeto Vehiculo con los parametros especificados.
      *
-     * @param patente: de tipo String,patente del vehículo.
-     * @param baul: de tipo boolean, Verdadero si el vehículo tiene espacio de carga (baúl), falso en caso contrario.
-     * @param petFriendly: de tipo boolean, Verdadero si el vehículo es apto para mascotas, falso en caso contrario.
-     * @param maxPasajeros: de tipo int,número máximo de pasajeros que puede transportar el vehículo.
+     * @param patente: de tipo String,patente del vehiculo.
+     * @param baul: de tipo boolean, Verdadero si el vehículo tiene espacio de carga (baul), falso en caso contrario.
+     * @param petFriendly: de tipo boolean, Verdadero si el vehiculo es apto para mascotas, falso en caso contrario.
+     * @param maxPasajeros: de tipo int,numero maximo de pasajeros que puede transportar el vehiculo.
      */
     protected Vehiculo(String patente, boolean baul, boolean petFriendly, int maxPasajeros) {
         this.patente = patente;
@@ -27,7 +26,7 @@ public abstract class Vehiculo {
     }
 
   
-	/**Calculara valor de prioridad de un para un pedido en cuestión.
+	/**Calculara valor de prioridad de un para un pedido en cuestion.
 	 * @param pedido: de tipo Pedido, datos de un pedido en particular
 	 * @return int con el valor de prioridad que tiene un vehiculo respecto a un pedido
 	 */
@@ -43,25 +42,25 @@ public abstract class Vehiculo {
     }
 	/** Verifica si se pueden transportar cierta cantidad de pasajeros
 	 * <b> Pre:</b> cantidadPasajero debe ser mayor a cero<br>
-     * <b> Post:</b>compara la cantidad de pasajeros del pedido con el maximo numero de personas que se pueden
-     * transportar en el vehículo 
-	 * @param cantidadPasajeros: de tipo int, número de pasajeros del pedido
-	 * @return boolean que indica si el vehúculo puede llevar a la cantidad de gente del pedido
+     * <b> Post:</b> compara la cantidad de pasajeros del pedido con el maximo numero de personas que se pueden
+     * transportar en el vehiculo 
+	 * @param cantidadPasajeros: de tipo int, numero de pasajeros del pedido
+	 * @return boolean que indica si el vehiculo puede llevar a la cantidad de gente del pedido
 	 */
 	protected boolean Verifica_Cant_Pas(int cantidadPasajeros)
 	    {    return cantidadPasajeros<=this.maxPasajeros;}
 	    
-    /** Método abstracto que se impermentaré según sea necesario, debe verificar si se puede acceder 
-     * al servicio de baúl deseado
-     * @return boolean que indica si se puede brindar el servicio necesario de baúl
+    /** Metodo abstracto que se impermentara segun sea necesario, debe verificar si se puede acceder 
+     * al servicio de baul deseado
+     * @return boolean que indica si se puede brindar el servicio necesario de baul
      */
     protected abstract boolean VerificaBaul(boolean deseaBaul);
-    /** Mútodo abstracto que se impermentaá según sea necesario, debe verificar si se puede acceder 
+    /** Metodo abstracto que se impermentara segun sea necesario, debe verificar si se puede acceder 
      * al servicio PetFriendly deseado
      * @return boolean que indica si se puede brindar el servicio PetFriendly requerido
      */
     protected abstract boolean VerificaPetFriendly(boolean deseaPetFrienly);
-    /** Metodo abstracto que calcula el valor de la prioridad de un vehículo de acuerdo a un pedido
+    /** Metodo abstracto que calcula el valor de la prioridad de un vehiculo de acuerdo a un pedido
      * 
      * @param pedido:de tipo Pedido, datos de un pedido en particular
      * @return int con valor de prioridad asignado
@@ -76,15 +75,15 @@ public abstract class Vehiculo {
 			return patente;
 		}
 
-	/**Informa si el vehículo tiene espacio de carga (baul)
+	/**Informa si el vehiculo tiene espacio de carga (baul)
 	 * @return boolean que indica si se cuenta con espacio de carga.
 	 */
 	public boolean isBaul() {
 			return baul;
 		}
 
-	/**Informa si el vehículo es apto para mascotas
-	 * @return boolean que indica si si el vehículo es apto para mascotas
+	/**Informa si el vehiculo es apto para mascotas
+	 * @return boolean que indica si si el vehiculo es apto para mascotas
 	 */
 	public boolean isPetFriendly() {
 			return petFriendly;
