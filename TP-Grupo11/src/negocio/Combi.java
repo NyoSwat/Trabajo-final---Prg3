9be1685a796f41f58aceebd609a9cd9a6d3f2a45
Eugenia <164356917+Eugtoledo@users.noreply.github.com>
2024-05-04 10:31:46 -0300

    JavaDoc

--- TP-Grupo11/src/negocio/Combi.java
+++ TP-Grupo11/src/negocio/Combi.java
@@ -1,27 +1,27 @@
 package negocio;
 
 /**
- * La clase Combi representa un vehiculo tipo combi.
- * Hereda de la clase Vehiculo y establece que tiene espacio para pasajeros adicionales y espacio en el baul.
+ * La clase Combi representa un vehículo tipo combi.
+ * Hereda de la clase Vehiculo y establece que tiene espacio para pasajeros adicionales y espacio en el baúl.
  */
 public class Combi extends Vehiculo {
 
     /**
      * Constructor para crear un objeto Combi.
      *
-     * @param patente La patente de la combi.
+     * @param patente : de tipo String con la patente de la combi.
      */
     public Combi(String patente) {
         super(patente, true, false, 10);
     }
-    /**Verificar si se puede acceder al servicio de baul deseado.
+    /**Verifica si se puede acceder al servicio de baul deseado.
      * @return boolean true ya que este servicio esta disponible se solicite o no.
      */
 	protected boolean VerificaBaul(boolean deseaBaul) {
 		return true;
 	}
 
-	/**Verificar si se puede acceder al servicio PetFrienly deseado
+	/**Verifica si se puede acceder al servicio PetFrienly deseado
      * @return boolean que indica si se puede brindar el servicio necesario de traslado de mascotas
      */
 	protected boolean VerificaPetFriendly(boolean deseaPetFrienly) {
@@ -32,10 +32,10 @@ public class Combi extends Vehiculo {
 	}
 
 	
-	/** Metodo abstracto que calcula el valor de la prioridad de un vehículo de acuerdo a un pedido
+	/** Método abstracto que calcula el valor de la prioridad de un vehiculo de acuerdo a un pedido
      * <b> Pre:</b> pedido.isPetFriendly()=false <br>
      *              pedido.getCantPasajeros()<=10 <br>
-     * <b> Post:se asigna el valor de prioridad correspondiente, segun se trate de un viaje que necesite
+     * <b> Post:se asigna el valor de prioridad correspondiente, según se trate de un viaje que necesite
      * baul o no</b>
      * @param pedido:de tipo Pedido, datos de un pedido en particular
      * @return int con valor de prioridad asignado
@@ -50,7 +50,11 @@ public class Combi extends Vehiculo {
         	return pedido.getCantPasajeros()*ptosPas;
 	}
 	
-	@Override
+	/**
+	 * Otorga formato a Combi
+	 * @return String representa al objeto Combi
+	 */
+	
 	public String toString() {
 		return "\n**********"+
 				"\nCombi"+super.toString();
