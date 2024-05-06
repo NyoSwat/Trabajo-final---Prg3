da10bbc688ae29fad7490426ea754c9fe2d1d0a1
NyoSwat <nicolaseliavicoli@gmail.com>
2024-05-05 23:57:23 -0300

    comentarios a excepciones

--- TP-Grupo11/src/Excepciones/VehiculoExistenteException.java
+++ TP-Grupo11/src/Excepciones/VehiculoExistenteException.java
@@ -1,7 +1,15 @@
 package Excepciones;
 
-public class VehiculoExistenteException extends Exception{
-	public VehiculoExistenteException(String msj) {
-		super(msj);
-	}
+/**
+ * Excepción personalizada que se lanza cuando se intenta agregar un vehículo existente.
+ */
+public class VehiculoExistenteException extends Exception {
+    /**
+     * Crea una nueva instancia de VehiculoExistenteException con un mensaje de error específico.
+     *
+     * @param msj El mensaje de error que describe la razón de la excepción.
+     */
+    public VehiculoExistenteException(String msj) {
+        super(msj);
+    }
 }
