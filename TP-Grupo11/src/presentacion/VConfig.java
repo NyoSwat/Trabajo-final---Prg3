9f88f6ddfd552835a6b7dc8d59218ffbc5fa2837
Facundo Criado <facu-1999-44@hotmail.com>
2024-05-31 01:44:00 -0300

    Update VConfig.java

--- TP-Grupo11/src/presentacion/VConfig.java
+++ TP-Grupo11/src/presentacion/VConfig.java
@@ -18,6 +18,8 @@ import java.awt.Color;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VConfig extends JFrame {
 
@@ -26,9 +28,9 @@ public class VConfig extends JFrame {
 	private JTextField cantClienteField;
 	private JTextField cantPedidoField;
 	private JTextField patenteField;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField dniField;
+	private JTextField nombreField;
+	private JTextField cantHijosField;
 
 	/**
 	 * Launch the application.
@@ -149,10 +151,6 @@ public class VConfig extends JFrame {
 		addVehiculoBtn.setBounds(181, 75, 81, 21);
 		vehiculoPanel.add(addVehiculoBtn);
 		
-		JComboBox typeVehiculoOption = new JComboBox();
-		typeVehiculoOption.setModel(new DefaultComboBoxModel(new String[] {"Automovil", "Combi", "Moto"}));
-		typeVehiculoOption.setBounds(70, 25, 100, 21);
-		vehiculoPanel.add(typeVehiculoOption);
 		
 		patenteField = new JTextField();
 		patenteField.setBounds(70, 53, 76, 20);
@@ -169,6 +167,23 @@ public class VConfig extends JFrame {
 		petFriendlyChkBox.setBounds(8, 102, 119, 21);
 		vehiculoPanel.add(petFriendlyChkBox);
 		
+		JComboBox typeVehiculoComboBox = new JComboBox();
+		typeVehiculoComboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(typeVehiculoComboBox.getSelectedItem().equals("Moto")) {
+					baulChkBox.setVisible(false);
+					petFriendlyChkBox.setVisible(false);
+				}
+				else {
+					baulChkBox.setVisible(true);
+					petFriendlyChkBox.setVisible(true);
+				}
+			}
+		});
+		typeVehiculoComboBox.setModel(new DefaultComboBoxModel(new String[] {"Automovil", "Combi", "Moto"}));
+		typeVehiculoComboBox.setBounds(70, 25, 100, 21);
+		vehiculoPanel.add(typeVehiculoComboBox);
+
 		JPanel chofePanel = new JPanel();
 		chofePanel.setBounds(0, 305, 424, 140);
 		titlePanel.add(chofePanel);
@@ -184,55 +199,70 @@ public class VConfig extends JFrame {
 		listChoferes.setBounds(270, 1, 154, 139);
 		chofePanel.add(listChoferes);
 		
-		JLabel contratadoLabel = new JLabel("Categoria");
-		contratadoLabel.setBounds(10, 30, 70, 15);
-		chofePanel.add(contratadoLabel);
+		JLabel categoriaLabel = new JLabel("Categoria");
+		categoriaLabel.setBounds(10, 30, 70, 15);
+		chofePanel.add(categoriaLabel);
 		
-		JLabel permanenteLabel = new JLabel("DNI");
-		permanenteLabel.setBounds(10, 55, 70, 15);
-		chofePanel.add(permanenteLabel);
+		JLabel dniLabel = new JLabel("DNI");
+		dniLabel.setBounds(10, 55, 70, 15);
+		chofePanel.add(dniLabel);
 		
-		JLabel TemporarioLabel = new JLabel("Nombre");
-		TemporarioLabel.setBounds(10, 80, 70, 15);
-		chofePanel.add(TemporarioLabel);
+		JLabel nombreLabel = new JLabel("Nombre");
+		nombreLabel.setBounds(10, 80, 70, 15);
+		chofePanel.add(nombreLabel);
 		
-		JButton addContratadoBtn = new JButton("Agregar");
-		addContratadoBtn.setBounds(181, 85, 81, 21);
-		chofePanel.add(addContratadoBtn);
+		JButton addChoferBtn = new JButton("Agregar");
+		addChoferBtn.setBounds(181, 85, 81, 21);
+		chofePanel.add(addChoferBtn);
 		
 		JButton deleteChoferBtn = new JButton("Eliminar");
 		deleteChoferBtn.setBounds(181, 116, 81, 21);
 		chofePanel.add(deleteChoferBtn);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Contratado", "Permanente", "Temporario"}));
-		comboBox.setBounds(80, 25, 100, 21);
-		chofePanel.add(comboBox);
 		
-		textField = new JTextField();
-		textField.setBounds(80, 55, 76, 20);
-		chofePanel.add(textField);
-		textField.setColumns(10);
+		dniField = new JTextField();
+		dniField.setBounds(80, 55, 76, 20);
+		chofePanel.add(dniField);
+		dniField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(80, 80, 76, 20);
-		chofePanel.add(textField_1);
-		textField_1.setColumns(10);
+		nombreField = new JTextField();
+		nombreField.setBounds(80, 80, 76, 20);
+		chofePanel.add(nombreField);
+		nombreField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Cantidad hijos");
-		lblNewLabel.setBounds(8, 105, 92, 15);
-		chofePanel.add(lblNewLabel);
+		JLabel cantHijosLabel = new JLabel("Cantidad hijos");
+		cantHijosLabel.setBounds(8, 105, 92, 15);
+		chofePanel.add(cantHijosLabel);
+		cantHijosLabel.setVisible(false);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(100, 105, 55, 20);
-		chofePanel.add(textField_2);
-		textField_2.setColumns(10);
+		cantHijosField = new JTextField();
+		cantHijosField.setBounds(100, 105, 55, 20);
+		chofePanel.add(cantHijosField);
+		cantHijosField.setColumns(10);
+		cantHijosField.setVisible(false);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBounds(0, 452, 424, 42);
 		titlePanel.add(buttonPanel);
 		buttonPanel.setLayout(null);
 		
+		JComboBox categoriaComboBox = new JComboBox();
+		categoriaComboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(categoriaComboBox.getSelectedItem().equals("Permanente")) {
+					cantHijosField.setVisible(true);
+					cantHijosLabel.setVisible(true);
+				}
+				else{
+					cantHijosField.setVisible(false);
+					cantHijosLabel.setVisible(false);
+				}
+			}
+		});
+		categoriaComboBox.setModel(new DefaultComboBoxModel(new String[] {"Contratado", "Permanente", "Temporario"}));
+		categoriaComboBox.setBounds(80, 25, 100, 21);
+		chofePanel.add(categoriaComboBox);
+
 		JButton saveBtn = new JButton("Guardar Datos");
 		saveBtn.setBounds(150, 5, 120, 25);
 		buttonPanel.add(saveBtn);
